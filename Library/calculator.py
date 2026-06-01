a2f7f5c0e89fdc6fee9d35863eb8b6b3726a723d
thaShi23 <thashi2309.tk@gmail.com>
2026-06-02 03:03:25 +0900

    Update calculator.py

--- Library/calculator.py
+++ Library/calculator.py
@@ -1,2 +1,32 @@
-def subtract (a, b):
-    return a - b
+print("The Calculator")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("which operation you want to do (1/2/3/4): ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == "1":
+    result = num1 + num2
+    print("Result =", result)
+
+elif choice == "2":
+    result = num1 - num2
+    print("Result =", result)
+
+elif choice == "3":
+    result = num1 * num2
+    print("Result =", result)
+
+elif choice == "4":
+    if num2 != 0:
+        result = num1 / num2
+        print("Result =", result)
+    else:
+        print("Cannot divide by zero!")
+
+else:
+    print("Invalid")
